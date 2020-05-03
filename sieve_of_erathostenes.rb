17af746f80e7e49db41c98bbeac934cade78bd46
Ivaylo Pashov <ivo.pashoff@gmail.com>
2020-05-03 11:13:09 +0300

    Sieve of Eratosthene modifications
    I got this one a bit wrong:
    - 1 is not condired a prime
    - my description was not correct. We are not finding
    the first n primes but the primes up to n
    - i applied a small optimization. Only need to iterate up to
    sqrt(n). Others are taken care of with the lower numbers.

--- sieve_of_erathostenes.rb
+++ sieve_of_erathostenes.rb
@@ -1,18 +1,19 @@
-# find the first N prime numbers
+# find prime numbers in interval
 
 def sieve_of_eratosthenes(n)
   sieve = n.times.map { true }
-  sieve[0] = false
+  sieve[0], sieve[1] = false, false
 
-  sieve.each_with_index do |value, index|
-    next if index == 0 || index == 1
-    next unless value
+  2.upto(Math.sqrt(sieve.size)) do |index|
+    next unless sieve[index]
 
-    inner_index = index + index
+    counter = 0
+    non_prime_index = index * (index + counter)
 
-    while inner_index < sieve.size
-      sieve[inner_index] = false
-      inner_index += index
+    while non_prime_index < sieve.size
+      sieve[non_prime_index] = false
+      counter += 1
+      non_prime_index = index * (index + counter)
     end
   end
 
@@ -22,4 +23,4 @@ def sieve_of_eratosthenes(n)
     compact
 end
 
-p sieve_of_eratosthenes(100) == [1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+p sieve_of_eratosthenes(100) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
